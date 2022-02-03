c65a3a53a80d5c10cfe17bf937434b9d079f1e15
Peter Walkwitz <peter.walkwitz@my.simpson.edu>
2022-02-03 09:41:07 -0600

    updated roll a ball with blender obstacles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,8 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
 
+    private bool touchingTank = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,21 +48,37 @@ public class PlayerController : MonoBehaviour
         if(count >= pickupObjects.transform.childCount)
         {
             //winTextObject.text = "You beat level " + SceneManager.GetActiveScene().buildIndex.ToString();
-            Debug.LogError(winTextObject);
-            Debug.LogError(winTextObject.GetComponent<TextMeshPro>());
-            Debug.LogError(winTextObject.GetComponent<Text>().text);
+            //Debug.LogError(winTextObject);
+            //Debug.LogError(winTextObject.GetComponent<TextMeshPro>());
+            //Debug.LogError(winTextObject.GetComponent<Text>().text);
 
-            winTextObject.GetComponent<Text>().text = "You beat level ";
+            //winTextObject.GetComponent<Text>().text = "You beat level ";
             winTextObject.SetActive(true);
             Invoke("LoadNextScene", 2.0f);
         }
     }
+
+    //void Update()
+    //{
+    //    if (touchingTank)
+    //    {
+    //        rb.AddForce(-movementX * 3 * speed, 0f, -movementY * 3 * speed);
+    //        Debug.LogError(movementY + "tank3" + movementX);
+    //    }
+    //}
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX * speed, 0.0f, movementY * speed);
 
         rb.velocity = movement;
+        //rb.AddForce(movement);
+
+        //if (touchingTank)
+        //{
+        //    rb.AddForce(-movementX * 3 * speed, 0f, -movementY * 3 * speed);
+        //    Debug.LogError(movementY + "tank2" + movementX);
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
@@ -72,7 +90,27 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
+        //if (other.gameObject.CompareTag("Tank"))
+        //{
+        //    // This stuff doesn't work (if the on trigger is clicked then it can run thro the tank)
+        //    Debug.LogError(movementY + "tank" + movementX);
+        //    rb.AddForce(-movementX * 3 * speed, 0f, -movementY * 3 * speed);
+        //    Debug.LogError(movementY + "tank" + movementX);
+        //}
+        //if (other.gameObject.CompareTag("Star"))
+        //{
+        //    rb.AddForce(-movementX, 0f, -movementY);
+        //    Debug.LogError("star");
+        //}
     }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Tank")
+    //    {
+    //        touchingTank = false;
+    //    }
+    //}
 
     private void LoadNextScene()
     {
